390bb8d9512d53fba8a69165906c1bc69a8f894d
David Ko <david.ko@pvtmethod.com>
2017-10-25 12:22:32 -0400

    wip: Formula for Faktory lib

---
+++ Formula/faktory.rb
@@ -0,0 +1,34 @@
+class Faktory < Formula
+  desc "Faktory is a language-agnostic, persistent background job system."
+  homepage "https://github.com/contribsys/faktory"
+  url "https://github.com/contribsys/faktory/archive/v0.5.0.tar.gz"
+  version "0.5.0"
+  sha256 "388eefa0834ff71803a3353db5ae336e73246e1ca2a5866c218e8172a253fceb"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test faktory`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
